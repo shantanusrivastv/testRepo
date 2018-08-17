23fda0c012076ba7a1919b21864bd8c1b995ba29
Shantanu <kshantanu@sapient.com>
2018-08-17 13:45:29 +0530

    5th

--- TestRepo/Program.cs
+++ TestRepo/Program.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestRepo
+﻿namespace TestRepo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
-
-
         }
     }
 }
